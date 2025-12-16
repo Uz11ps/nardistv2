9af6ca44cc9971ee95b5a0edce081d6149d6e631
rxnydash1337 <abcronydash1337@gmail.com>
2025-12-16 15:59:33 +0300

    фывафыва

--- backend/scripts/prisma-generate.sh
+++ backend/scripts/prisma-generate.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
-# Запускаем генерацию Prisma - Prisma сама определит все настройки
+# Добавляем путь к OpenSSL библиотекам в LD_LIBRARY_PATH для Ubuntu
+export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:${LD_LIBRARY_PATH:-}
+
+# Запускаем генерацию Prisma - Prisma сама найдет библиотеки
 npx prisma generate "$@"
 
