b2ac18d5ce9d931654df96e63a29441771d5a4ef
rxnydash1337 <abcronydash1337@gmail.com>
2025-12-16 05:57:34 +0300

    Update trainer.module.ts

--- backend/src/trainer/trainer.module.ts
+++ backend/src/trainer/trainer.module.ts
@@ -3,9 +3,10 @@ import { TrainerController } from './trainer.controller';
 import { TrainerService } from './trainer.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { SubscriptionModule } from '../subscription/subscription.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, SubscriptionModule],
+  imports: [PrismaModule, SubscriptionModule, AuthModule],
   controllers: [TrainerController],
   providers: [TrainerService],
   exports: [TrainerService],
